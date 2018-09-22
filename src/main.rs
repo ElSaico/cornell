29d49464f75b58f760ae58199799329a6f95ada8
Bruno Marques <bruno@bmarques.net>
2018-09-22 01:20:08 -0300

    Added vga_buffer unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 #![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
 
 extern crate bootloader_precompiled;
 extern crate spin;
 extern crate volatile;
 #[macro_use]
 extern crate lazy_static;
+#[cfg(test)]
+extern crate std;
+#[cfg(test)]
+extern crate array_init;
 
 use core::panic::PanicInfo;
 
 #[macro_use]
 mod vga_buffer;
 
+#[cfg(not(test))]
 #[panic_handler]
 #[no_mangle]
 pub fn panic(_info: &PanicInfo) -> ! {
@@ -19,6 +25,7 @@ pub fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World!");
