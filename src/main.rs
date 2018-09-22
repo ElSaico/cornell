e376a55b9f6dd9fe6ccac892a1e67f716085dcd9
Bruno Marques <bruno@bmarques.net>
2018-09-22 01:06:41 -0300

    Added basic VGA buffer support

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,25 @@
 #![no_main]
 
 extern crate bootloader_precompiled;
+extern crate spin;
+extern crate volatile;
+#[macro_use]
+extern crate lazy_static;
 
 use core::panic::PanicInfo;
+
+#[macro_use]
+mod vga_buffer;
 
 #[panic_handler]
 #[no_mangle]
 pub fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    println!("Hello World!");
     loop {}
 }
